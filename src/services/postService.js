365d1242a008cf17337a776123a5c215e4468c83
rkgroup813-art <rkgroup813@gmail.com>
2026-04-28 23:31:54 +0530

    Create src/services/postService.js file with post functions

---
+++ src/services/postService.js
@@ -0,0 +1,37 @@
+// postService.js
+
+/**
+ * Create a new post.
+ * @param {Object} post - The post data.
+ * @returns {Promise} - Promise representing the created post.
+ */
+export const createPost = async (post) => {
+    // Implementation here
+};
+
+/**
+ * Get all posts.
+ * @returns {Promise} - Promise representing the list of posts.
+ */
+export const getPosts = async () => {
+    // Implementation here
+};
+
+/**
+ * Like a post.
+ * @param {string} postId - The ID of the post to like.
+ * @returns {Promise} - Promise representing the updated post.
+ */
+export const likePost = async (postId) => {
+    // Implementation here
+};
+
+/**
+ * Add a comment to a post.
+ * @param {string} postId - The ID of the post.
+ * @param {string} comment - The comment to add.
+ * @returns {Promise} - Promise representing the updated post with the new comment.
+ */
+export const addComment = async (postId, comment) => {
+    // Implementation here
+};
